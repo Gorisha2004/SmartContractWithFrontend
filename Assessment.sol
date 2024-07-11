10f98977e26d1b2e02a25858c29411334bb1b659
Gorisha2004 <157053342+Gorisha2004@users.noreply.github.com>
2024-07-11 22:26:00 +0530

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -6,20 +6,42 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public lastTransactionTimestamp;
+    uint256 public minimumBalance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event BalanceIncreased(uint256 amount);
-    event BalanceDecreased(uint256 amount);
+    event MinimumBalanceSet(uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        lastTransactionTimestamp = block.timestamp;
+        minimumBalance = 0;
     }
 
     function getBalance() public view returns(uint256){
         return balance;
     }
+
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+
+    function getLastTransactionTimestamp() public view returns(uint256) {
+        return lastTransactionTimestamp;
+    }
+
+    function setMinimumBalance(uint256 _minimumBalance) public onlyOwner {
+        minimumBalance = _minimumBalance;
+        emit MinimumBalanceSet(_minimumBalance);
+    }
+
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
@@ -59,19 +81,5 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
-
-    function increaseBalance(uint256 _amount) public {
-    require(msg.sender == owner, "You are not the owner");
-    balance += _amount;
-    
-    emit BalanceIncreased(_amount);
-}
-
-    function decreaseBalance(uint256 _amount) public {
-    require(msg.sender == owner, "You are not the owner");
-    require(balance >= _amount, "Insufficient balance");
-    balance -= _amount;
-    emit BalanceDecreased(_amount);
-}
 
 }
